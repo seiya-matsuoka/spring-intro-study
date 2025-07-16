2cce50d2dcd26237ac39ce8a46dea2e64a0d9b9c
seiya matsuoka <seiya.matsuoka0413@gmail.com>
2025-07-16 22:09:04 +0900

    feat: Thymeleafの使用例を追加
    値結合、比較、分岐、オブジェクトを使用した場合の表示確認を追加

--- thymeleaf/src/main/java/com/example/demo/controller/ThymeleafController.java
+++ thymeleaf/src/main/java/com/example/demo/controller/ThymeleafController.java
@@ -4,11 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.entity.Member;
+
 @Controller
 public class ThymeleafController {
+	
 	@GetMapping("show")
 	public String showView(Model model) {
+		Member member = new Member(1, "会員01");
 		model.addAttribute("name", "太郎");
+		model.addAttribute("mb", member);
 		return "main";
 	}
 }
